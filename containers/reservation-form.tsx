ec5ef199c80089f4452b44b60f99d402c9a7fa01
Vojtech Petrus <vojtech.petrus@gmail.com>
2022-02-20 18:31:30 +0100

    Fixed styling for customer list

--- containers/reservation-form.tsx
+++ containers/reservation-form.tsx
@@ -34,40 +34,46 @@ const ReservationForm = () => {
 					</select>
 				</div>
 			</div>
-			<fieldset className="row mb-3">
-				<legend className="col-form-label col-sm-2 pt-0">Customers:</legend>
-				<div className="col-sm-10">
-					<p className="d-flex align-items-center">
-						Albus Dumbledore
-						<button type="button" className="btn btn-sm btn-outline-danger ms-3">
-							<i className="bi bi-trash"></i>
-						</button>
-					</p>
-					<p className="d-flex align-items-center">
-						John Mayer
-						<button type="button" className="btn btn-sm btn-outline-danger ms-3">
-							<i className="bi bi-trash"></i>
-						</button>
-					</p>
-					<div className="col-6">
-						<div className="input-group mb-3">
-							<input
-								type="text"
-								className="form-control"
-								placeholder="Customer's full name"
-								aria-label="Customer's full name"
-								aria-describedby="button-addon-customer"
-							/>
-							<button className="btn btn-primary" type="button" id="button-addon-customer">
-								Add customer
+			<fieldset className="form-group">
+				<div className="row">
+					<legend className="col-form-label col-sm-2 pt-0">Customers:</legend>
+					<div className="col-sm-10">
+						<p className="d-flex align-items-center">
+							Albus Dumbledore
+							<button type="button" className="btn btn-sm btn-outline-danger ms-3">
+								<i className="bi bi-trash"></i>
 							</button>
+						</p>
+						<p className="d-flex align-items-center">
+							John Mayer
+							<button type="button" className="btn btn-sm btn-outline-danger ms-3">
+								<i className="bi bi-trash"></i>
+							</button>
+						</p>
+						<div className="col-lg-6">
+							<div className="input-group mb-3">
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Customer's full name"
+									aria-label="Customer's full name"
+									aria-describedby="button-addon-customer"
+								/>
+								<button className="btn btn-primary" type="button" id="button-addon-customer">
+									Add customer
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
 			</fieldset>
-			<button type="submit" className="btn btn-primary offset-sm-2" disabled>
-				Create reservation
-			</button>
+			<div className="row">
+				<div className="offset-sm-2">
+					<button type="submit" className="btn btn-primary" disabled>
+						Create reservation
+					</button>
+				</div>
+			</div>
 		</form>
 	);
 };
